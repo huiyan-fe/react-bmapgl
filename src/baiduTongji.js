fd4db94dcb5bc11b5e8bd83618d524755857884c
Junior2Ran <hdr01@126.com>
2022-12-19 17:37:35 +0800

    update mapvgl 170

--- src/baiduTongji.js
+++ src/baiduTongji.js
@@ -1,7 +1,9 @@
-window._hmt = window._hmt || [];
-(function () {
-    var hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?b97569d26a525941d8d163729d284198';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+if (!window._hmt_private) {
+    window._hmt = window._hmt || [];
+    (function () {
+        var hm = document.createElement('script');
+        hm.src = 'https://hm.baidu.com/hm.js?b97569d26a525941d8d163729d284198';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+}
